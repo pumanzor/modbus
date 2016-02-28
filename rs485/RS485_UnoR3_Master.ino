1a1e452e7418bc0e607640a03162f33a5681a38a
Pablo Umanzor A <info@redlibre.cl>
2016-02-28 00:42:41 -0300

    Create RS485_UnoR3_Master.ino

---
+++ rs485/RS485_UnoR3_Master.ino
@@ -0,0 +1,42 @@
+#include <SimpleModbusMaster.h>
+
+#define baud 9600
+#define timeout 1000
+#define polling 200
+#define retry_count 10
+
+#define TxEnablePin 2 
+
+#define LED 9
+
+#define TOTAL_NO_OF_REGISTERS 1
+
+enum
+{
+  PACKET1,
+  PACKET2,
+  TOTAL_NO_OF_PACKETS // leave this last entry
+};
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0; 
+
+Packet packets[TOTAL_NO_OF_PACKETS];
+
+unsigned int regs[TOTAL_NO_OF_REGISTERS];
+
+void setup()
+{
+  modbus_construct(&packets[PACKET1], 1, READ_HOLDING_REGISTERS, 0, 1, 0);
+  modbus_construct(&packets[PACKET2], 1, PRESET_MULTIPLE_REGISTERS, 1, 1, 0);
+  
+  modbus_configure(&Serial, baud, SERIAL_8N2, timeout, polling, retry_count, TxEnablePin, packets, TOTAL_NO_OF_PACKETS, regs);
+  
+  pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+  modbus_update();
+  regs[0] = analogRead(0);
+  analogWrite(LED, regs[0]>>2); 
+}
