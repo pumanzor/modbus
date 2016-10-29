362fce9604bb1acac32b0a775d8e932ea5c4b522
Pablo Umanzor A <info@redlibre.cl>
2016-10-29 16:55:30 -0300

    Update RS485_UnoR3_Master.ino

--- rs485/RS485_UnoR3_Master.ino
+++ rs485/RS485_UnoR3_Master.ino
@@ -17,8 +17,6 @@ enum
   PACKET2,
   TOTAL_NO_OF_PACKETS // leave this last entry
 };
-int sensorValue = 0;        // value read from the pot
-int outputValue = 0; 
 
 Packet packets[TOTAL_NO_OF_PACKETS];
 
@@ -27,7 +25,7 @@ unsigned int regs[TOTAL_NO_OF_REGISTERS];
 void setup()
 {
   modbus_construct(&packets[PACKET1], 1, READ_HOLDING_REGISTERS, 0, 1, 0);
-  modbus_construct(&packets[PACKET2], 1, PRESET_MULTIPLE_REGISTERS, 1, 1, 0);
+  modbus_construct(&packets[PACKET2], 1, PRESET_MULTIPLE_REGISTERS, 0, 1, 0);
   
   modbus_configure(&Serial, baud, SERIAL_8N2, timeout, polling, retry_count, TxEnablePin, packets, TOTAL_NO_OF_PACKETS, regs);
   
